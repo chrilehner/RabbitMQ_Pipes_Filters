a8a914cc16d647d777f307e6dcdafc765b24074f
Christian Lehner <lehner.chri@gmail.com>
2015-05-28 13:45:49 +0200

    refactored start filter script

--- start_filters.js
+++ start_filters.js
@@ -1,21 +1,23 @@
+var fs = require('fs');
+var yaml = require('yamljs')
 var amqp = require('amqplib');
 
-var firstFilter = 'lower_case';
+var config = yaml.parse(fs.readFileSync('config.yml').toString());
+console.log(config);
+var firstFilter = config.lower_case.from;
 
 amqp.connect('amqp://localhost').then(function(connection) {
 	return connection;
 }).then(function(connection) {
 	return connection.createChannel();
 }).then(function(channel) {
-	channel.assertExchange(firstFilter, 'direct', { durable: false, autoDelete: true });
-
-	// durable true to survive broker restarts (not necessarily needed but the queue doesn't have to be created every time the broker restarts)
-	// exclusive and auto_delete set to false by default (not needed)
-	channel.assertQueue(firstFilter, { durable: false, autoDelete: true });
+	channel.assertExchange(firstFilter, 'direct', { durable: true });
+	channel.assertQueue(firstFilter, { durable: true });
 
 	channel.bindQueue(firstFilter, firstFilter);
 
 	channel.publish(firstFilter, '', new Buffer("!DLROW OLLEH"));
+	console.log("Message sent to", firstFilter);
 }).catch(function(e) {
 	console.error(e);
 });
